1ef0b8e17203a259a73bf93a02f978c0b3e35f35
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-05-24 16:14:19 -0300

    Update Linha_de_Transmissao.m

--- Trabalho 1/Linha_de_Transmissao.m
+++ Trabalho 1/Linha_de_Transmissao.m
@@ -1,9 +1,9 @@
-Rl = inputdlg('Entre resistência da carga: ','Sample',[1 5]);
-m = inputdlg('Entre tensão da fonte','Sample',[1 5]);#modo Vs m=1,2
+Rl = inputdlg('Entre resistÃªncia da carga: ','Sample',[1 5]);
+m = inputdlg('Entre tensÃ£o da fonte','Sample',[1 5]);#modo Vs m=1,2
 Rl = str2num(Rl{1});#resistencia da carga
-m = str2num(m{1});#modo da fonte de tensão (1 ou 2)
+m = str2num(m{1});#modo da fonte de tensÃ£o (1 ou 2)
 
-#qualqur valor diferente de 1 ou 2 é considerado 2
+#qualqur valor diferente de 1 ou 2 Ã© considerado 2
 if (m != 1 && m != 2)
   m = 2;
 endif
@@ -19,18 +19,18 @@ function y = Vs(t,m,uf,Z)
   endif
 endfunction
 
-Rs = 75; #resistência fonte
-Z0 = 50 ; #resistência da linha
+Rs = 75; #resistÃªncia fonte
+Z0 = 50 ; #resistÃªncia da linha
 c = 3e+8; #velocidade da luz
-uf = 0.9*c; #velocidade de propagação do sinal
-#valores acima na descrição do projeto 
+uf = 0.9*c; #velocidade de propagaÃ§Ã£o do sinal
+#valores acima na descriÃ§Ã£o do projeto 
 
 Z = 10;  # comprimento da linha
-T = 10*Z/uf;  #tempo limite, na decrição do projeto 
+T = 10*Z/uf;  #tempo limite, na decriÃ§Ã£o do projeto 
 
 L = Z0/uf; #Z0 = L* Uf
 C = L/Z0**2; #Z0 = sqrt(L/C)
-#L e C são derivados de uf e Z0
+#L e C sÃ£o derivados de uf e Z0
 
 dz = Z/1e+4;
 z = -Z:dz:0;
@@ -43,13 +43,13 @@ i = v = zeros( 2 , Lz );
 k1 = 0.5*(Rs*C*dz/dt - 1);
 k2 = 0.5*(Rs*C*dz/dt + 1);
 
-#cria o gráfico
+#cria o grÃ¡fico
 figure('Name',['Rl = ',num2str(Rl),', m = ', num2str(m)],'NumberTitle','off');
 V = v(1,:);
 I = i(1,:);
 subplot(2,1,1);
 plot (z, V,'ydatasource','V');
-title('Tensão');
+title('TensÃ£o');
 xlabel("z (m)");
 ylabel("v (V)");
 axis([-Z 0 -0.6/m 1.1*m]);
@@ -62,15 +62,15 @@ axis([-Z 0 -0.012/m 0.02*m]);
 w = waitbar(0,'t = 0s');
 
 tic
-M = 80;#1 em cada 100 tempos sao plotados, maior eficiência
+M = 80;#1 em cada 80 tempos sao plotados, maior eficiÃªncia
 for n = 2:Lt-1
-  #condição de fronteira z = -Z (fonte)
+  #condiÃ§Ã£o de fronteira z = -Z (fonte)
   v(2,1) = (k1/k2)*v(1,1) - (Rs*i(1,1) - (Vs((n+1/2)*dt,m,uf,Z) + Vs((n-1/2)*dt,m,uf,Z))/2)/k2;
   
-  #equação de update tensão, linha sem perdas
+  #equaÃ§Ã£o de update tensÃ£o, linha sem perdas
   v(2,2:Lz-1) = v(1,2:Lz-1) - dt*(i(1,2:Lz-1) - i(1,1:Lz-2))/(C*dz);
   
-  #condição de fronteira z = 0 (carga)
+  #condiÃ§Ã£o de fronteira z = 0 (carga)
   if (Rl == 0)
     v(2,Lz) = 0;
     i(2,Lz) = i(2,Lz-1);
@@ -82,14 +82,14 @@ for n = 2:Lt-1
     i(2,Lz) = v(2,Lz)/Rl;
   endif  
   
-  #equação de update corrente, linha sem perdas
+  #equaÃ§Ã£o de update corrente, linha sem perdas
   
   i(2,1:Lz-1) = i(1,1:Lz-1) - dt*(v(2,2:Lz)-v(2,1:Lz-1))/(L*dz);
   
   v(1,:) = v(2,:);
   i(1,:) = i(2,:);
   
-  #atualiza o gráfico, 1 em cada M iterações, grande aumento em performance.
+  #atualiza o grÃ¡fico, 1 em cada M iteraÃ§Ãµes, grande aumento em performance.
   if ( mod(n,M) == 0) 
     I = i(1,:);
     V = v(1,:);
