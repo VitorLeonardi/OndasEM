bb4b1d0ee94a21950efe6a061afce4919d0de983
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-06-30 00:51:19 -0300

    Update Ex2.4.m

--- Trabalho 2/Ex2.4.m
+++ Trabalho 2/Ex2.4.m
@@ -1,6 +1,5 @@
 %Ex 2.4 
 S = 1/sqrt(2);
-c = 3e+8;
 nt = 2*pi*S/acos(1-2*S**2); #2.30
 
 d = 0.001;
@@ -14,10 +13,11 @@ Vp2 = 2*pi./(N2.*acos(1+4*(cos(pi./N2)-1))); #2.32c
 N = [N1 N2];
 Vp = [Vp1 Vp2];
 
-plot(N,Vp,'b','linewidth',1);
-str = "Velocidade de fase";
-xlabel("Grid Sampling Density");
-ylabel("Numerical phase velocity ( normalized to c ) ");
-annotation('textbox',[.7 .4 .3 .3],'String',str);
+z = 1 + (cos(2*pi*S./N1)-1)/S**2; #2.29b
+r = -log(-z - sqrt(z.**2 - 1)); #2.37b
 
-clear
+plotyy(N1,r,N,Vp);
+xlabel("Grid Sampling Density");
+ylabel(ax(1),"Attenuation constant( nepers / grid cell )");
+ylabel(ax(2),"Numerical phase velocity ( normalized to c ) ");
+
