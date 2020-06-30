150abb7005333635dd70a57a0df73aa05e2fc784
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-06-30 01:30:41 -0300

    Add files via upload

---
+++ Trabalho 2/Ex2.6.m
@@ -0,0 +1,18 @@
+#Ex 2.5 
+
+S = 1/sqrt(2);
+c = 3e+8;
+nt = 2*pi*S/acos(1-2*S**2); #2.30
+
+d = 0.001;
+N = nt+d:d:80;
+
+Vp = 2*pi./(N.*acos(1+4*(cos(pi./N)-1))); #2.32c , normalizado
+
+Er = (1 - Vp)*100 ;
+
+semilogy(N,Er);
+title("S = 1/sqrt(2)");
+xlabel("Grid Sampling Density");
+ylabel("Phase velocity error ( % )");
+
