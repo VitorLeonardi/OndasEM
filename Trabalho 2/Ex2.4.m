1cf624c482709474a7b55ca0e422cce5397e19bd
JulioEO <40305649+JulioEO@users.noreply.github.com>
2020-07-11 13:40:35 -0300

    Update Ex2.4.m

--- Trabalho 2/Ex2.4.m
+++ Trabalho 2/Ex2.4.m
@@ -8,7 +8,7 @@ N1 = 1:d:nt;
 N2 = nt+d:d:10;
 
 Vp1 = 2./N1; #2.37a
-Vp2 = 2*pi./(N2.*acos(1+4*(cos(pi./N2)-1))); #2.32c
+Vp2 = 2*pi./(N2.*acos(1+(1/S)**2 *(cos((2*pi*S)./N2)-1))); #2.32c
 
 N = [N1 N2];
 Vp = [Vp1 Vp2];
